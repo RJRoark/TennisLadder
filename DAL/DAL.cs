0bde8ad691074b34c4fdfe11e71d8b81f7e6f672
RJRoark <rj.roarkjr@gmail.com>
2015-03-15 18:23:03 -0400

    Add

---
+++ DAL/DAL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NDatabase;
+
+namespace DAL
+{
+    public class DAL
+    {
+
+        public static void Store(object data)
+        {
+
+            // Open the database
+            using (var odb = OdbFactory.Open("test.db"))
+            {
+                // Store the object
+                odb.Store(data);
+            }
+        }
+
+        public static object GetPlayer()
+        {
+            using (var odb1 = OdbFactory.Open("test.db"))
+            {
+                var player = odb1.QueryAndExecute<object>();
+                return player.First().ToString();
+            }
+
+           
+        }
+        
+    }
+}
---
+++ DALTest/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+
+namespace DALTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass RJ = new TestClass();
+            RJ.ID = 1;
+            RJ.FirstName = "RJ";
+            RJ.MiddleInitial = "J";
+            RJ.LastName = "Roark";
+
+            TestClass xx;
+
+            xx = (TestClass) DAL.DAL.GetPlayer();
+
+         }
+    }
+
+    class TestClass
+    {
+        private int _ID;
+       
+        public int ID
+        {
+            get { return _ID; }
+            set { _ID = value; }
+        }
+
+        private string _lastName;
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        private string _middleInit;
+
+        public string MiddleInitial
+        {
+            get { return _middleInit; }
+            set { _middleInit = value; }
+        }
+
+        private string _firstName;
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        
+    }
+}
